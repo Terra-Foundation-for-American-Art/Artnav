f57c72bcc8edbb01772984eeed24cdf8bb3b8b2d
Trenisha <taglee09@gmail.com>
2023-09-08 15:24:49 -0400

    add view more published artmaps under main menu

--- client/src/components/artmap/MainMenu.vue
+++ client/src/components/artmap/MainMenu.vue
@@ -14,28 +14,23 @@
           <br>
           <p v-if="local_data.art.description" class='about_artwork'> Image Description: {{ local_data.art.description }}</p>
         </div>
-        <!-- <div class='collection_list' v-if='local_data.collections.length'>
-          <div class='row no-gutters'><p class='related-section-label'>Related:</p></div>
-          <div class='row related_collection no-gutters' v-if='local_data.collections' v-for='(collection, i) in local_data.collections' :key='i'>
-            <h2 class='col-sm-12'>{{collection.collection_title}}</h2>
-            <p v-if='collection.curatorial_statement !== "None"' class='curatorial_statement col-sm-12' v-html='collection.curatorial_statement'></p>
-            <p class='external_link col-sm-12' v-if='collection.external_link !== "None"'>
-              <a :href='collection.external_link' target='blank'>Read More <span class="oi oi-chevron-right"></span></a>
-            </p>
+        <div class='collection_list' v-if='published_artworks'>
+          <div class='row no-gutters'><p class='related-section-label'> View More:</p></div>
+          <div class='row related_collection no-gutters'>
             <div
-              v-if='collection.artworks.length'
+              v-if='all_artwork'
               class='col-sm-3 related-artwork'
-              v-for='(artwork, j) in collection.artworks'
+              v-for='(artwork, j) in published_artworks'
               :key='j'
-              @click='goToArtmap(artwork.slug)'>
+              @click='goToArtmap(artwork.artwork_slug)'>
               <div
                 class='artwork_img'
-                :style='"background-image:url("+artwork.url+");"'></div>
-              <h1 class='collection_artwork_title'>{{artwork.name}}</h1>
-              <p>by {{artwork.artist}}</p>
+                :style='"background-image:url("+artwork.thumbnail+");"'></div>
+              <h1 class='collection_artwork_title'>{{artwork.artwork_title}}</h1>
+              <p>by {{artwork.artist.artist_name}}</p>
             </div>
           </div>
-        </div> -->
+        </div>
       </div>
     </div>
   </transition>
@@ -43,10 +38,14 @@
 <script>
 import {QuillDeltaToHtmlConverter} from 'quill-delta-to-html'
 import {mapState} from 'vuex'
+import { axiosInstance } from '@/api/endpoints'
 export default {
   data () {
     return {
-      open: false
+      open: false,
+      all_artwork: null,
+      all_artwork_backup: null,
+      published_artworks: null,
     }
   },
   computed: {
@@ -62,7 +61,59 @@ export default {
     },
     goToArtmap: function (slug) {
       window.location.href = `/gallery/${slug}`
-    }
+    },
+    getCatalogData: function () {
+      var updatedArtwork = []
+
+      this.all_artwork.forEach((artItem) => {
+        updatedArtwork.push(artItem)
+      })
+
+      updatedArtwork.forEach((updatedArtItem, i) => {
+        axiosInstance.get(`https://dlc.services/iiif-img/3/2/${updatedArtItem.iiif_uuid}/info.json`)
+          .then(iiif_resp => {
+            updatedArtItem['iiif'] = iiif_resp.data
+            updatedArtItem['thumbnail'] = `https://dlc.services/thumbs/3/2/${updatedArtItem.iiif_uuid}/full/${iiif_resp.data.sizes[1].width},${iiif_resp.data.sizes[1].height}/0/default.jpg`
+          }, err => {
+            console.log(err)
+            updatedArtItem['api_error'] = err
+          })
+      })
+
+      if (updatedArtwork.length) {
+        var asyncCheckerArtwork = setInterval(() => {
+          var count = 0
+          var arraySize = updatedArtwork.length
+          updatedArtwork.forEach(item => {
+            if (item.iiif || item.api_error) { count++ }
+            if (count === arraySize) {
+              clearInterval(asyncCheckerArtwork)
+              this.all_artwork = updatedArtwork
+              this.all_artwork_backup = updatedArtwork
+              this.loading = false
+            }
+          })
+        }, 50)
+      } else {
+        this.loading = false
+      }
+    },
+    getAllArtwork: function () {
+      this.loading = true
+      axiosInstance.get('all-artwork/')
+        .then(resp => {
+          let n = 3;
+          this.all_artwork = resp.data
+          this.published_artworks = this.all_artwork.filter(artwork => artwork.published === true)
+          this.published_artworks = this.published_artworks.sort(() => Math.random() - Math.random()).slice(0, n)
+          this.getCatalogData()
+        }, err => {
+          console.log(err)
+        })
+    },
+  },
+  mounted() {
+    this.getAllArtwork()
   }
 }
 </script>
@@ -127,7 +178,7 @@ export default {
     padding-top:1.6rem;
   }
   .related-section-label{
-    @include font-size(0.8);
+    @include font-size(1);
     color:$colors_grays_light;
 
   }
@@ -157,7 +208,7 @@ export default {
       cursor: pointer;
       margin-top:1rem;
       .artwork_img{
-        background-size:cover;
+        background-size: cover;
         background-repeat: no-repeat;
         width:7rem;
         height:7rem;
@@ -178,7 +229,7 @@ export default {
         }
       }
       h1{
-        @include font-size(1.2);
+        @include font-size(1);
         @include line-height(1.8);
         margin-bottom:0.3rem;
       }
